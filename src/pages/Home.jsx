4ff31745df56898257a5934a770ee53baab644ac
Juan Carlos Fedyna <jfedyna@outlook.com>
2026-07-07 19:03:12 -0300

    poniendole onda a las tarjetas

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -3,12 +3,21 @@ import Card from "../components/Card";
 import cards from "../data/cards";
 import "../styles/home.css";
 
+import {
+    HiOutlineChatBubbleLeftRight,
+    HiOutlineClipboardDocumentList,
+    HiOutlineUsers,
+    HiOutlinePresentationChartLine,
+    HiOutlineUserCircle,
+} from "react-icons/hi2";
+
 function Home() {
     return (
         <Layout>
             <div className="home">
                 <section className="hero-card">
                     <div className="hero-inner">
+
                         <h1>Acompañamiento institucional</h1>
 
                         <h2>
@@ -20,11 +29,85 @@ function Home() {
                             propuesta de acuerdo con la realidad de la institución.
                         </p>
 
-                        <img
-                            src="/images/acompania.PNG"
-                            alt="Acompañamiento Logos"
-                            className="hero-image"
-                        />
+                        {/* ACOMPAÑAMIENTO */}
+
+                        <div className="acompanamiento-wrapper">
+
+                            <div className="acompanamiento-grid">
+
+                                <div className="acompanamiento-card">
+                                    <div className="numero">1</div>
+
+                                    <HiOutlineChatBubbleLeftRight className="acompanamiento-icon" />
+
+                                    <h3>Escucha institucional</h3>
+
+                                    <p>
+                                        Conocemos necesidades, niveles, tiempos y
+                                        prioridades.
+                                    </p>
+                                </div>
+
+                                <div className="acompanamiento-card">
+                                    <div className="numero">2</div>
+
+                                    <HiOutlineClipboardDocumentList className="acompanamiento-icon" />
+
+                                    <h3>Presentación y planificación</h3>
+
+                                    <p>
+                                        Presentamos el programa y definimos juntos el
+                                        recorrido de trabajo.
+                                    </p>
+                                </div>
+
+                                <div className="acompanamiento-card">
+                                    <div className="numero">3</div>
+
+                                    <HiOutlineUsers className="acompanamiento-icon" />
+
+                                    <h3>Organización y gestión</h3>
+
+                                    <p>
+                                        Coordinamos agendas, invitaciones, materiales,
+                                        envíos y accesos.
+                                    </p>
+                                </div>
+
+                                <div className="acompanamiento-card">
+                                    <div className="numero">4</div>
+
+                                    <HiOutlinePresentationChartLine className="acompanamiento-icon" />
+
+                                    <h3>Implementación acompañada</h3>
+
+                                    <p>
+                                        Sostenemos el contacto y acompañamos el
+                                        desarrollo del programa.
+                                    </p>
+                                </div>
+
+                            </div>
+
+                            {/* REFERENTE */}
+
+                            <div className="referente-box">
+
+                                <div className="referente-icon-wrapper">
+                                    <HiOutlineUserCircle className="referente-react-icon" />
+                                </div>
+
+                                <p>
+                                    Un referente comercial coordina el proceso y articula
+                                    con el equipo pedagógico para garantizar una
+                                    implementación efectiva y sostenida.
+                                </p>
+
+                            </div>
+
+                        </div>
+
+                        {/* PROGRAMAS */}
 
                         <div className="hero-programs">
                             {cards.map((card) => (
@@ -46,6 +129,7 @@ function Home() {
                                 </div>
                             ))}
                         </div>
+
                     </div>
                 </section>
             </div>
