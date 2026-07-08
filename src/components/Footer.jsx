1d6b918c32389e4849df604a0eced038699df9d2
Juan Carlos Fedyna <jfedyna@outlook.com>
2026-07-08 10:54:19 -0300

    Actualiza Footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,11 +3,11 @@ import "../styles/footer.css";
 function Footer() {
     return (
         <footer className="footer">
-            <img
+            {/* <img
                 src="/images/logos-footer.png"
                 alt="footer"
                 className="footer-image"
-            />
+            /> */  }
             <p className="footer-copy">
                 © {new Date().getFullYear()} LOGOS. Todos los derechos reservados.
             </p>
